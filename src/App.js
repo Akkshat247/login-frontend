677ee9fd54a15c985809b5b85d338551ab30cc68
AkkshatSPM <95082971+AkkshatSPM@users.noreply.github.com>
2026-04-04 16:21:40 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await fetch("http://16.52.121.213:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+    alert(data.success ? "Login success" : "Login failed");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 50 }}>
+      <h2>Login</h2>
+      <input placeholder="Username" onChange={e => setUsername(e.target.value)} /><br /><br />
+      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} /><br /><br />
+      <button onClick={login}>Login</button>
     </div>
   );
 }
